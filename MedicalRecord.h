bb3d5b9b76f8c25cb7910691da7ab3d48d9f3511
justinkieu1125 <duykieu@csu.fullerton.edu>
2017-02-28 19:50:55 -0800

    Update MedicalRecord.h

--- MedicalRecord.h
+++ MedicalRecord.h
@@ -10,12 +10,13 @@ class MedicalRecord {
 public:
 	// default constructor
 	MedicalRecord() {
-		// TO BE COMPLETED
+		totalbirths = 0;
+		lowestweight = 0;
 	}
 
 	// destructor
 	~MedicalRecord() {
-		// TO BE COMPLETED
+		cout << "Array destroyed." << endl;
 	}
 
 	// Load information from a text file with the given filename.
@@ -39,28 +40,58 @@ public:
 
 	// return the most frequently appearing name in the text file
 	string mostPopularName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
+		int searchArray[1000];
+		for (int i = 0; i < totalbirths; i++)
+		{
+			for (int j = 0; j < totalbirths; j++)
+			{
+				if (array[i].getName() == array[j].getName())
+				{
+					searchArray[i]++;
+				}
+			}
+		}
+
+		int  popularName = 0;
+		int max = searchArray[0];
+		for (int i = 1; i < totalbirths; i++)
+		{
+			if (searchArray[i] > max)
+			{
+				max = searchArray[i];
+				popularName = i;
+			}
+		}
+		return array[popularName].getName(); 
 	}
 
 	// return the number of baby records loaded from the text file
 	int numberOfBirths() {
-		return -1; // TO BE COMPLETED
+		return totalbirths; 
 	}
 
 	// return the number of babies who had birth weight < 2500 grams
 	int numberOfBabiesWithLowBirthWeight() {
-		return -1; // TO BE COMPLETED
+		return lowestweight; 
 	}
 
 	// return the number of babies who have the name contained in string s
 	int numberOfBabiesWithName(string s) {
-		return -1; // TO BE COMPLETED
+		int names = 0;
+		for (int i = 0; i < totalbirths; i++)
+		{
+			if (array[i].getName() == s)
+				names++;
+		}
+		return names; 
 	}
 
 private:
-	// update the data structure with information contained in Baby object
+	int totalbirths, lowestweight;
+	Baby array[1000];
 	void addEntry(Baby b) {
-		// TO BE COMPLETED
+		array[totalbirths] = b;
+		totalbirths++;
 	}
 
 	// Add private member variables for your data structure along with any 
